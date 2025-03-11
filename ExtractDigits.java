6ee82341cc80037d6cf540b18b02cfbee92bfd8b
BiditG <biditgiri123@gmail.com>
2025-03-11 08:22:39 +0545

    Ëxtract digits problem

---
+++ ExtractDigits.java
@@ -0,0 +1,12 @@
+import java.util.regex.*;
+    
+public class ExtractDigits {
+    public static void main(String[] args) {
+        String text = "abc123def456";
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
