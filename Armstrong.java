23139bc2fffac3ed97dd2d1ffc7c43a8ed3ae6b5
BiditG <biditgiri123@gmail.com>
2025-03-20 20:31:41 +0545

    Fixed some errors

--- Armstrong.java
+++ Armstrong.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
-public class Armstrong {       public static void main(String[] args) {
-            int num = 153;
-            int sum = 0;
-            int n = num;
-            while (n > 0) {
-                 int r = n % 10;
-                 sum = sum + (int) Math.pow(r, 3);
-                 
 
-            }
-         }int n = num;
-while (n > 0) {
-    int r = n % 10;
-    sum = sum + (int) Math.pow(r, 3);
-    n = n / 10;
-}
-if (sum == num)
-    System.out.println(num + " is an Armstrong number");
-else
-    System.out.println(num + " is not an Armstrong number");
+
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 153;
+        int sum = 0;
+        int n = num;
+        while (n > 0) {
+            int r = n % 10;
+            sum = sum + (int) Math.pow(r, 3);
+            n = n / 10;
+        }
+        if (sum == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+};
