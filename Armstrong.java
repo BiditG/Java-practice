42348516f7ad360d2322357998f92ac02115e511
BiditG <biditgiri123@gmail.com>
2025-04-10 21:26:47 +0545

    Changes

--- Armstrong.java
+++ Armstrong.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
-        int num = 102009;
+        int num = 1009;
         int sum = 0;
         int n = num;
         while (n > 0) {
